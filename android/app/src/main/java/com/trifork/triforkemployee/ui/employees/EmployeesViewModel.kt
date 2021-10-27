35cbf2bd3b1a7d4dc67c172481f4f39816326672
Jørgen Christian Heinsen <c.heinsen@hotmail.com>
2021-10-27 10:15:08 +0200

    Added EmployeesViewModel.

---
+++ android/app/src/main/java/com/trifork/triforkemployee/ui/employees/EmployeesViewModel.kt
@@ -0,0 +1,26 @@
+package com.trifork.triforkemployee.ui.employees
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.trifork.triforkemployee.database.employee.Employee
+import com.trifork.triforkemployee.repo.employee.EmployeeRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class EmployeesViewModel @Inject constructor(
+    employeeRepository: EmployeeRepository
+) : ViewModel() {
+
+    private val _employees = MutableLiveData<List<Employee>>()
+    val employees : LiveData<List<Employee>> = _employees
+
+    init {
+        viewModelScope.launch {
+            _employees.value = employeeRepository.getEmployees()
+        }
+    }
+}
