5606df9ca1ac5d77491fdd3181abf8b5a26691d2
Jørgen Christian Heinsen <c.heinsen@hotmail.com>
2021-11-03 20:04:49 +0100

    Finished EventsViewModel.

--- android/app/src/main/java/com/trifork/triforkemployee/ui/events/EventsViewModel.kt
+++ android/app/src/main/java/com/trifork/triforkemployee/ui/events/EventsViewModel.kt
@@ -3,11 +3,26 @@ package com.trifork.triforkemployee.ui.events
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.trifork.triforkemployee.database.Employee
+import com.trifork.triforkemployee.database.Event
+import com.trifork.triforkemployee.repo.EventRepository
+import com.trifork.triforkemployee.repo.employee.EmployeeRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class EventsViewModel : ViewModel() {
+@HiltViewModel
+class EventsViewModel @Inject constructor(
+    eventRepository: EventRepository
+) : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is events Fragment"
+    private val _events = MutableLiveData<List<Event>>()
+    val events: LiveData<List<Event>> = _events
+
+    init {
+        viewModelScope.launch {
+            _events.value = eventRepository.getEvents()
+        }
     }
-    val text: LiveData<String> = _text
 }
